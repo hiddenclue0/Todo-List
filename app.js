e8ea8c25b30fae4336c6c6149aea58f3057a29ac
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-08 13:10:43 +0600

    added Todo List Project

---
+++ app.js
@@ -0,0 +1,35 @@
+const list = [];
+let input = prompt("What would you like to do?");
+while (true) {
+  if (input.toLowerCase() === "new") {
+    let items = prompt("Enter new todo");
+    list.push(items);
+  } else if (input.toLowerCase() === "list") {
+    if (list.length) {
+      console.log(`***************`);
+      for (let i = 0; i < list.length; i++) {
+        console.log(`${i}: ${list[i]}`);
+      }
+      console.log(`***************`);
+    } else {
+      console.log("Your Todo List is Empty");
+      input = prompt("Your Todo List is Empty\nWhat would you like to do?");
+      continue;
+    }
+  } else if (input.toLowerCase() === "delete") {
+    if (list.length) {
+      let index = parseInt(prompt("Enter index of todo to delete"));
+      list.splice(index, 1);
+    } else {
+      console.log("Your Todo List is Empty\nNothing to delete");
+      input = prompt(
+        "Your Todo List is Empty\nNothing to delete\nWhat would you like to do?"
+      );
+      continue;
+    }
+  } else if (input.toLowerCase() === "quit") {
+    console.log("You Quit This App");
+    break;
+  }
+  input = prompt("What would you like to do?");
+}
