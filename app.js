31e184f5c9488e0512b5c9ae7203dd330dae8799
Md Jakir Hosen <hiddenclue0@gmail.com>
2025-01-08 14:34:11 +0600

    modified app.js

--- app.js
+++ app.js
@@ -1,43 +1,67 @@
 const list = [];
 let input = prompt("What would you like to do?");
 while (true) {
+  //If inputs new
   if (input.toLowerCase() === "new") {
     let items = prompt("Enter new todo");
     list.push(items);
-  } else if (input.toLowerCase() === "list") {
+  }
+
+  //If inputs list
+  else if (input.toLowerCase() === "list") {
+    //If Available list
     if (list.length) {
       console.log(`***************`);
       for (let i = 0; i < list.length; i++) {
         console.log(`${i}: ${list[i]}`);
       }
       console.log(`***************`);
-    } else {
+    }
+
+    //If list doesn't exist
+    else {
       console.log("Your Todo List is Empty");
       input = prompt("Your Todo List is Empty\nWhat would you like to do?");
       continue;
     }
-  } else if (input.toLowerCase() === "delete") {
+  }
+
+  //If inputs delete
+  else if (input.toLowerCase() === "delete") {
+    //If list exist
     if (list.length) {
       let index = parseInt(prompt("Enter index of todo to delete"));
+
+      //check the user inputs correct index
       while (Number.isNaN(index)) {
         index = prompt("Enter index of todo to delete");
       }
+
+      //check the user index exixt or not
       if (index <= list.length && index.length >= 0) {
         list.splice(index, 1);
       } else {
         console.log("Index doesn't exist");
         input = prompt("Index doesn't exist\nWhat would you like to do?");
       }
-    } else {
+    }
+
+    //If list doesn't exist
+    else {
       console.log("Your Todo List is Empty\nNothing to delete");
       input = prompt(
         "Your Todo List is Empty\nNothing to delete\nWhat would you like to do?"
       );
       continue;
     }
-  } else if (input.toLowerCase() === "quit") {
+  }
+
+  //If inputs quit
+  else if (input.toLowerCase() === "quit") {
     console.log("You Quit This App");
     break;
   }
+
+  //repeating
   input = prompt("What would you like to do?");
 }
